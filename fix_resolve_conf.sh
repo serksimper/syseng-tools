03aefa74ee49b00f44f66b1f66dc8b59c07565b8
serksimper <serksimper@gmail.com>
2022-09-06 18:27:28 -0600

    New shell script to fix WSL /etc/resolv.conf issue

---
+++ fix_resolve_conf.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+num_lines=$(wc -l </etc/resolv.conf)
+
+if [ "$num_lines" -lt 4 ]; then
+  echo -e "nameserver 8.8.8.8" >>/mnt/infidel/wsl/resolv.conf
+  echo -e "Added DNS Resolvers to /etc/resolv.conf"
+else
+  echo -e "Nothing to do here, Bob!"
+fi
