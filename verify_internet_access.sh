4c186bdff9388b6049d8314ad8c9701577fbb67e
serksimper <serksimper@gmail.com>
2022-09-06 18:52:15 -0600

    fixed some issues with resolv.conf script

--- verify_internet_access.sh
+++ verify_internet_access.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
+#set -x
 # bashsupport disable=BP5008,BP5008,BP2001
 # bashsupport disable=BP5008
 # bashsupport disable=BP5008
-function check_internet() {
 
-  internet_status="UNKNOWN"
-  timestamp=$(date +%s)
-  while true; do
-    ping -c 1 -W 8.8.8.8 >/dev/null 2>&1
-    if [ $? -eq 0 ]; then
-      if [ "$internet_status" != "UP" ]; then
-        echo "UP   $(date +%Y-%m-%dT%H:%M:%S%Z) $(($(date +%s) - timestamp))"
-        internet_status="UP"
-      fi
-    else
-      if [ "$internet_status" = "UP" ]; then
-        echo "DOWN $(date +%Y-%m-%dT%H:%M:%S%Z) $(($(date +%s) - timestamp))"
-        internet_status="DOWN"
-      fi
+internet_status="UNKNOWN"
+timestamp=$(date +%s)
+while true; do
+  ping -c 1 -W 8.8.8.8 >/dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    if [ "$internet_status" != "UP" ]; then
+      echo "UP   $(date +%Y-%m-%dT%H:%M:%S%Z) $(($(date +%s) - timestamp))"
+      internet_status="UP"
     fi
-    sleep 1
-  done
-}
+  else
+    if [ "$internet_status" = "UP" ]; then
+      echo "DOWN $(date +%Y-%m-%dT%H:%M:%S%Z) $(($(date +%s) - timestamp))"
+      internet_status="DOWN"
+    fi
+  fi
+  sleep 1
 
-function main() {
-  check_internet
-}
-
-main "$@"
+done
+#set +x
